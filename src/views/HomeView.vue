58859b02c21438520865f8bffb8d0dae4a4f05e2
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-10-01 18:05:30 +0500

    tailwind css init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,7 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <h1>Hello world</h1>
+    <h1 class="text-amber-400">Hello world</h1>
   </main>
 </template>
