12684013b435a0ecf4c622d7acb8b9cc2dc789b4
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-10-01 17:54:40 +0500

    update app

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
   </header>
 
   <RouterView />
 </template>
 
 <style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
 nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
+  margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
@@ -45,6 +58,10 @@ nav a:first-of-type {
     padding-right: calc(var(--section-gap) / 2);
   }
 
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
   header .wrapper {
     display: flex;
     place-items: flex-start;
@@ -55,7 +72,9 @@ nav a:first-of-type {
     text-align: left;
     margin-left: -1rem;
     font-size: 1rem;
+
     padding: 1rem 0;
+    margin-top: 1rem;
   }
 }
 </style>
