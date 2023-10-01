b70712a941e6dc178dbdd7daedcb5c6495049f49
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-10-01 17:59:19 +0500

    set app port

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  server: { port: 3000 },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: { port: 3000 }
+  }
 })
